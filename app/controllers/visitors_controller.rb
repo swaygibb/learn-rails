bdef8062e75cc38c77ca8fadd92c175a62b6c754
Steve Gibbs <swaygibb@gmail.com>
2020-01-18 12:44:14 -0600

    mailing list

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,12 +1,21 @@
 class VisitorsController < ApplicationController
     def new
-        Rails.logger.debug 'DEBUG: entering new method'
-        @owner = Owner.new
-        
-        #flash.now[:notice] = 'Welcome!'
-        #flash.now[:alert] = 'My birthday is soon.'
-
-        Rails.logger.debug 'DEBUG: Owner name is ' + @owner.name
-        # raise 'GIBBS FAILURE'
+        @visitor = Visitor.new
     end
-end
+
+    def create
+        @visitor = Visitor.new(secure_params)
+        if @visitor.valid?
+            @visitor.subscribe
+            flash[:notice] = "Signed up #{@visitor.email}."
+            redirect_to root_path
+        else
+            render :new
+        end
+    end
+    private
+
+    def secure_params
+        params.require(:visitor).permit(:email)
+    end
+  end
