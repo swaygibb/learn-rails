782e93e34d8cd1e90e7e295b826633b68890cfcc
Steve Gibbs <swaygibb@gmail.com>
2020-01-17 21:55:41 -0600

    update application layout

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,6 +2,8 @@ class VisitorsController < ApplicationController
     def new
         Rails.logger.debug 'DEBUG: entering new method'
         @owner = Owner.new
+        flash.now[:notice] = 'Welcome!'
+        flash.now[:alert] = 'My birthday is soon.'
         Rails.logger.debug 'DEBUG: Owner name is ' + @owner.name
         # raise 'GIBBS FAILURE'
     end
